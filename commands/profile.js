f3fce8c96ef77038d6ed0b66ff3cc7424513e236
Chesser2017 <chesser201710@gmail.com>
2019-09-28 11:45:55 +0600

    profile now shows inventory

--- commands/profile.js
+++ commands/profile.js
@@ -8,11 +8,22 @@ module.exports = {
         let user = msg.mentions.users.first() || msg.author;
         let userBank = await fetchBank(user);
         let rubyEmoji = client.emojis.get('626941464991105057');
+        let inventory = JSON.parse(userBank.dataValues.inventory);
+        let string = '';
+        for(let i = 0; i < inventory.length; i++){
+            if(i != inventory.length - 1)
+                string += inventory[i] + " | ";
+            else{
+                string += inventory[i];
+            }
+        }
+        if(string === '') string = 'No Items';
         let reply = new Discord.RichEmbed()
                             .setAuthor(`${user.username}`, `${user.avatarURL}`)
                             .setDescription(`-----------------------------------------`)
                             .setThumbnail(`${user.avatarURL}`)
-                            .addField(`User Info:`, `Level\n${userBank.dataValues.level}\n\nXP\n${userBank.dataValues.current_xp}/${100 + ((userBank.dataValues.level - 1) * 75)}\n\nRubees${rubyEmoji}\n${userBank.dataValues.currency}`);
+                            .addField(`User Info:`, `Level\n${userBank.dataValues.level}\n\nXP\n${userBank.dataValues.current_xp}/${100 + ((userBank.dataValues.level - 1) * 75)}\n\nRubees${rubyEmoji}\n${userBank.dataValues.currency}`)
+                            .addField(`Inventory`, string);
         return msg.channel.send(reply);
     },
     aliases: ['p', 'prof'],
